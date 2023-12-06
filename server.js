44b9f900f6a8217f079bdd0624fb41dc5e5e5e62
Sara McCauley <144122640+sarahizar@users.noreply.github.com>
2023-12-05 21:50:27 -0600

    moved folders

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const apiRoutes = require('./routes/apiRoutes'); 
 const htmlRoutes = require('./routes/htmlRoutes')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
